d1b2dee1828c14afeef9bfa3d7beecac5c13a7af
Eduardo Gurgel <eduardo@gurgel.me>
2014-05-25 22:42:18 +1200

    Add list! and keys! to Riak.Bucket

--- lib/riak/bucket.ex
+++ lib/riak/bucket.ex
@@ -4,8 +4,26 @@ defmodule Riak.Bucket do
   def list(pid), do: list_buckets(pid)
   def list(pid, timeout), do: list_buckets(pid, timeout)
 
+  def list!(pid) do
+    {:ok, buckets} = list(pid)
+    buckets
+  end
+  def list!(pid, timeout) do
+    {:ok, buckets} = list(pid, timeout)
+    buckets
+  end
+
   def keys(pid, bucket), do: list_keys(pid, bucket)
   def keys(pid, bucket, timeout), do: list_keys(pid, bucket, timeout)
+
+  def keys!(pid, bucket) do
+    {:ok, keys} = keys(pid, bucket)
+    keys
+  end
+  def keys!(pid, bucket, timeout) do
+    {:ok, keys} = keys(pid, bucket, timeout)
+    keys
+  end
 
   def get(pid, bucket), do: get_bucket(pid, bucket)
   #Possible Props: [n_val: 3, allow_mult: false, last_write_wins: false, basic_quorum: false, notfound_ok: true, precommit: [], postcommit: [], pr: 0, r: :quorum, w: :quorum, pw: 0, dw: :quorum, rw: :quorum]}
