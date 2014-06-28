fcb4a9388b4aca1d5e3fd0d3ffd0b9e0c03beb10
Eduardo Gurgel <eduardo@gurgel.me>
2014-06-28 17:48:16 +1200

    Add Riex.Connection.start/2

--- lib/riex/connection.ex
+++ lib/riex/connection.ex
@@ -2,4 +2,8 @@ defmodule Riex.Connection do
   def start_link(host \\ '127.0.0.1', port \\ 8087) do
     :riakc_pb_socket.start_link(host, port)
   end
+
+  def start(host \\ '127.0.0.1', port \\ 8087) do
+    :riakc_pb_socket.start(host, port)
+  end
 end
