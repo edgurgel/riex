c10641897f4d2ab0f94b23a8bc25e3c424695f75
Eduardo Gurgel <eduardo@gurgel.me>
2014-07-08 21:44:02 +1200

    Use basho's rebar on riak_pb

--- mix.exs
+++ mix.exs
@@ -16,6 +16,7 @@ defmodule Riex.Mixfile do
   defp deps do
     [ {:pooler, github: "seth/pooler"},
       {:exlager, github: "khia/exlager"},
+      {:riak_pb, github: "basho/riak_pb", override: true, tag: "2.0.0.16", compile: "./rebar get-deps compile deps_dir=../"},
       {:riakc, github: "basho/riak-erlang-client"}]
   end
 end
